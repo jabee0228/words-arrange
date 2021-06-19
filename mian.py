67b1f03b3b49a0efac154897c901b5eaab3d9ade
jabee0228 <james930228@gmail.com>
2021-06-19 23:41:55 +0800

    update (bug)

--- mian.py
+++ mian.py
@@ -1,3 +1,8 @@
+
+
+def returnlastchar(s):
+    return s[10:]
+
 z = 1
 a = []
 for i in range(150):
@@ -6,5 +11,9 @@ for i in range(150):
         break
     a.append(z)
 a.sort()
+decorated = [(returnlastchar(s), s) for s in a]
+decorated.sort()
+a = [x[1] for x in decorated]
+
 for i in range(len(a)):
     print(a[i])
